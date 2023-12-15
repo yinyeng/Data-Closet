befaa1aabb8b2e46eff05da73ccfd90283b7d8bb
mlmarteja <marianmarteja@gmail.com>
2023-12-14 22:00:50 -0500

    modal maybe?

--- main.js
+++ main.js
@@ -1,36 +1,11 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".ARC");
+const closeModal = document.querySelector(".close-button");
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
-
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
-}
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
